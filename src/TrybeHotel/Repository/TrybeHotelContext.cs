6c5ac610d6cf4d89fbada9dc3dc2bdfbf9ea8249
Gabriel Puttin <gabrielputtin@hotmail.com>
2024-01-31 15:51:32 -0300

    Feat: req 01

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -5,5 +5,30 @@ namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
-    public TrybeHotelContext() { }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Hotel>()
+            .HasOne(h => h.City)
+            .WithMany(c => c.Hotels)
+            .HasForeignKey(h => h.CityId);
+
+        modelBuilder.Entity<Room>()
+            .HasOne(r => r.Hotel)
+            .WithMany(h => h.Rooms)
+            .HasForeignKey(r => r.HotelId);
+    }
 }
